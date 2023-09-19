8cceae72a87fed2f3e2a2b07c085027daa500034
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-09-19 10:19:18 +0900

    Update Nix.nix

--- Nix.nix
+++ Nix.nix
@@ -1,2 +1,13 @@
 $ #
 (nix-shell) $ 
+{ pkgs ? import <nixpkgs> {} # here we import the nixpkgs package set   
+     2  }:                                                                      
+     3  pkgs.mkShell {               # mkShell is a helper function             
+     4    name="dev-environment";    # that requires a name                     
+     5    buildInputs = [            # and a list of packages                   
+     6      pkgs.nodejs                                                         
+     7    ];                                                                    
+     8    shellHook = ''             # bash to run when you enter the shell     
+     9      echo "Start developing..."                                          
+    10    '';                                                                   
+    11  }                                                                       
